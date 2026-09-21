29e970bdfaf6f8f9820edf601774880132391862
Vicky Jadhav <76901120+spaciousejar@users.noreply.github.com>
2026-09-21 10:03:05 +0530

    fix: restore packaging pins to 1.4.2 after bad auto-bumps

--- Formula/vicine.rb
+++ Formula/vicine.rb
@@ -1,8 +1,8 @@
 class Vicine < Formula
   desc "Search, stream, and download movies, series and anime from the terminal"
   homepage "https://github.com/spaciousejar/vicine-cli"
-  url "https://github.com/spaciousejar/vicine-cli/archive/refs/tags/vmaster.tar.gz"
-  sha256 "02e141b6360ad200abcdeef6f0de69889e518e2aabbf753d3f50af0f0c18480c"
+  url "https://github.com/spaciousejar/vicine-cli/archive/refs/tags/v1.4.2.tar.gz"
+  sha256 "a57c7065f938123e41a65593ba3c4a5909adbc506694df2b18701d4a1cb3f3d5"
   license "GPL-3.0-or-later"
 
   depends_on "fzf"
--- flake.nix
+++ flake.nix
@@ -14,11 +14,11 @@
         in rec {
           default = pkgs.stdenvNoCC.mkDerivation {
             pname = "vicine";
-            version = "master";
+            version = "1.4.2";
 
             src = pkgs.fetchurl {
-              url = "https://github.com/spaciousejar/vicine-cli/archive/refs/tags/vmaster.tar.gz";
-              sha256 = "02e141b6360ad200abcdeef6f0de69889e518e2aabbf753d3f50af0f0c18480c";
+              url = "https://github.com/spaciousejar/vicine-cli/archive/refs/tags/v1.4.2.tar.gz";
+              sha256 = "a57c7065f938123e41a65593ba3c4a5909adbc506694df2b18701d4a1cb3f3d5";
             };
 
             dontBuild = true;
